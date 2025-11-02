6dbddf13d6e7120ec45d15712438ad8475f475b4
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-11-02 19:59:53 +0330

    Set up initial React Query configuration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { BrowserRouter} from "react-router-dom";
 import Router from "./router/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import defaultOptions from "./configs/reactQueryConfigs";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 function App() {
+
+  const queryClinet = new QueryClient({ defaultOptions }); 
+
   return (
-    <BrowserRouter>
-      <Router/>
-    </BrowserRouter>
+
+    <QueryClientProvider client={queryClinet} >
+
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+      <ReactQueryDevtools/>
+      
+    </QueryClientProvider>
   )
 }
 
