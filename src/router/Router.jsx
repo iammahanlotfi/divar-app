149109c066cbe914c2899615dd1376369beb008a
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-11-04 18:43:37 +0330

    Restrict user access to pages.

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from '../pages/HomePage'
 import DashboardPage from '../pages/DashboardPage'
 import AuthPage from '../pages/AuthPage'
@@ -17,9 +17,9 @@ function Router() {
   return (
     <Routes>
         <Route path="/" element = {<HomePage/>} />
-        <Route path="/dashboard" element = {<DashboardPage/>} />
-        <Route path="/auth" element = {<AuthPage/>} />
-        <Route path="/admin" element = {<AdminPage/>} />
+        <Route path="/dashboard" element = {data ? (<DashboardPage/>) : (<Navigate to="/auth" />)} />
+        <Route path="/auth" element = {data ? (<Navigate to="/dashboard" />) : (<AuthPage/>)} />
+        <Route path="/admin" element = {data && data.data.role === "ADMIN" ? (<AdminPage/>) : (<Navigate to="/" />)} />
         <Route path="/*" element = {<PageNotFound/>} />
     </Routes>
   )
