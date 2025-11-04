f0c9b50e19d64ccc1aac9dfb8bcbc6f52c5a033a
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-11-04 18:31:16 +0330

    Redirect user after login.

--- src/components/templates/CheckOtpForm.jsx
+++ src/components/templates/CheckOtpForm.jsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { checkOTP } from "../../services/auth";
 import { setCookie } from "../../utils/cookie";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "../../services/user";
 
 function CheckOtpForm({ code , setCode , setStep , mobile }) {
+    const navigate = useNavigate();
+    const {refetch} = useQuery(["profile"], getProfile);
+
+
 
     const submitHandler = async (event) => { 
         event.preventDefault() ; 
@@ -13,7 +20,8 @@ function CheckOtpForm({ code , setCode , setStep , mobile }) {
 
         if (response) { 
             setCookie(response.data); 
-            
+            navigate("/");
+            refetch();   
         }
         if (error) { 
             console.log(error.response.data.message) ;
