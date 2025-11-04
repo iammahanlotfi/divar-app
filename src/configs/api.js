b7788f596b332c68899d72f39a62eef3b9bb0f6b
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-11-04 12:22:45 +0330

    Fetch new access token using refresh token.

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { getCookie } from "../utils/cookie";
+import { getCookie, setCookie } from "../utils/cookie";
+import { getNewTokens } from "../services/token";
 
 const api = axios.create({
     baseURL : import.meta.env.VITE_BASE_URL,
@@ -17,6 +18,24 @@ api.interceptors.request.use((request) => {
 }, error => { 
     return Promise.reject(error) ; 
 });
+
+api.interceptors.response.use((response) => {
+    return response ; 
+} , async (error) => { 
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true ;
+        
+        const response = await getNewTokens() ;
+        if (!response?.response) return ; 
+        setCookie(response.response.data);
+
+        return api(originalRequest) ; 
+        
+    }
+})
+ 
 
 
 export default api ;
