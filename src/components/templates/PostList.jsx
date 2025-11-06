d5d79b7788ffecb64e44a346ba686cb9efffa2d1
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-11-06 16:57:01 +0330

    Display user ads in dashboard.

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -1,14 +1,35 @@
 import { useQuery } from "@tanstack/react-query"
 import { getposts } from "../../services/user";
+import Loader from "../modules/Loader";
 
 function PostList() {
-
+    const baseURL = import.meta.env.VITE_BASE_URL;
     const {data , isLoading} = useQuery(["my-post-list"] , getposts);
-    console.log(data); 
-     
-
     return (
-    <div>PostList</div>
+    <div>
+        {
+            isLoading ? <Loader/> : (
+                <>
+                    <h3>آگهی های شما</h3>
+                    {
+                        data.data.posts.map((post) => (
+                            <div key={post._id} >
+                                <img src={`${baseURL}${post.images[0]}`} />
+                                <div>
+                                    <p>{post.options.title}</p>
+                                    <span>{post.options.content}</span>
+                                </div>
+                                <div>
+                                    <p>{post.createdAt}</p>
+                                    <span>{post.amount} تومان</span>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </>
+            )
+        }
+    </div>
   )
 }
 
