bbe6db78b9181e373942e684d4be0c5e62d5d050
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-11-06 12:07:09 +0330

    Implement form handling and data management.

--- src/components/templates/AddPost.jsx
+++ src/components/templates/AddPost.jsx
@@ -1,18 +1,38 @@
 import { useQuery } from "@tanstack/react-query"
 import { getCategory } from "../../services/admin"
+import { useState } from "react";
 
 function AddPost() {
     const {data} = useQuery(["get-categories"] , getCategory) ;
-    console.log(data) ; 
+    
+    const [form , setForm] = useState({
+        title : "",
+        content : "" , 
+        category : "" , 
+        city : "" ,
+        amount : null , 
+        images : null , 
+    });
+
+    const changeHandler = (event) => {
+        const name = (event.target.name);
+
+        if (name !== "images") { 
+            setForm({...form , [name] : event.target.value}); 
+        }
+        else { 
+            setForm({...form , [name] : event.target.files[0]});  
+        }
+    };
 
     const addHandler = (event) => { 
         event.preventDefault() ; 
-        console.log("sent") ;
+        console.log(form) ;
 
     }
 
   return (
-    <form>
+    <form onChange={changeHandler} >
         <h3>افزودن آگهی</h3>
         <label htmlFor="title">عنوان</label>
         <input type="text" name="title" id="title" />
