de6cc44ac3da2e248bf0a4f55d99df355fd8b9fa
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-11-08 17:58:01 +0330

    Display all ads list.

--- src/components/templates/Main.jsx
+++ src/components/templates/Main.jsx
@@ -1,9 +1,26 @@
+import { sp } from "../../utils/numbers";
 
 function Main({ posts }) {
+
+    const baseURL = import.meta.env.VITE_BASE_URL ; 
+
     console.log(posts) ;
     
   return (
-    <div>Main</div>
+    <div>
+        {posts.data.posts.map((post) => (
+            <div key={post._id} >
+                <div>
+                    <p>{post.options.title}</p>
+                    <div>
+                        <p>{sp(post.amount)} تومان</p>
+                        <span>{post.options.city}</span>
+                    </div>
+                </div>
+                <img src={`${baseURL}${post.images[0]}`} />
+            </div>
+        ))}
+    </div>
   )
 }
 
