80aa74daa7b4bf3367344302bafd69eb872138e5
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-11-08 17:15:26 +0330

    Add CSS styling to sidebar.

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,12 +1,13 @@
 import { useQuery } from "@tanstack/react-query"
 import { getCategory } from "../../services/admin";
+import styles from "./Sidebar.module.css" ;
 
 function Sidebar() {
     const { data } = useQuery(["get-categories"] , getCategory) ;
     console.log(data) ; 
 
   return (
-    <div>
+    <div className={styles.sidebar} >
         <h4>دسته ها</h4>
         <ul>
             {
