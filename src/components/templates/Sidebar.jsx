4a70ad678baa90e3b9ac419084d097038b026c34
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-11-08 16:31:25 +0330

    Implement sidebar and categories.

--- src/components/templates/Sidebar.jsx
+++ src/components/templates/Sidebar.jsx
@@ -1,7 +1,24 @@
+import { useQuery } from "@tanstack/react-query"
+import { getCategory } from "../../services/admin";
 
 function Sidebar() {
+    const { data } = useQuery(["get-categories"] , getCategory) ;
+    console.log(data) ; 
+
   return (
-    <div>Sidebar</div>
+    <div>
+        <h4>دسته ها</h4>
+        <ul>
+            {
+                data?.data.map((category) => (
+                    <li key={category._id} >
+                        <img src={`${category.icon}.svg`} />
+                        <p>{category.name}</p>
+                    </li>
+                ))
+            }
+        </ul>
+    </div>
   )
 }
 
