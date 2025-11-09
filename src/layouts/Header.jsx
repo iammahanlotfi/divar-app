631882a8573676496727d7f71c5a49851ba04355
Mahan Lotfi <mahanlotfi85@gmail.com>
2025-11-09 13:33:12 +0330

    Add logout feature.

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from "./Header.module.css"
+import { useQuery } from '@tanstack/react-query';
+import { getProfile } from '../services/user';
+import { deleteCookie, getCookie } from '../utils/cookie';
 
 function Header() {
+    const accessToken = getCookie("accessToken");
+    const refreshToken = getCookie("refreshToken") ;
+
+    const navigate = useNavigate() ; 
+    const { refetch } = useQuery(["profile"] , getProfile) ; 
+    
+    const exitHandler = () => {
+        deleteCookie("accessToken");
+        deleteCookie("refreshToken");
+        navigate("/");
+        refetch();
+    }
   return (
     <header className={styles.header} >
         <div>
@@ -15,6 +30,7 @@ function Header() {
             </span>
         </div>
         <div>
+
             <Link to="/auth" >
                 <span>
                     <img src="profile.svg" />
@@ -22,6 +38,7 @@ function Header() {
                 </span>
             </Link>
             <Link to="/dashboard" className={styles.button} >ثبت آگهی</Link>
+            {accessToken && refreshToken && (<button className={styles.exitButton} onClick={exitHandler} >خروج</button>)}
         </div>
     </header>
   )
--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,11 +1,15 @@
 const setCookie = (tokens) => { 
-    document.cookie = `accessToken=${tokens.accessToken}; max-age=${1 *24 * 60 *60}`;
-    document.cookie = `refreshToken=${tokens.accessToken}; max-age=${30*24 * 60 *60}`;
+    document.cookie = `accessToken=${tokens.accessToken}; max-age=${1 *24 * 60 *60}; path=/`;
+    document.cookie = `refreshToken=${tokens.refreshToken}; max-age=${30*24 * 60 *60} path=/`;
 } ;
 
 const getCookie = (cookieName) => {  
    return document.cookie.split(";").find(token => token.trim().split("=")[0]===cookieName)?.split("=")[1];
 }
 
-export {setCookie, getCookie} ; 
+const deleteCookie = (cookieName) => {
+  document.cookie = `${cookieName}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+};
+
+export {setCookie, getCookie , deleteCookie} ; 
  
